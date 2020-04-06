f535220cb5c223f9c1d4310eff74b3464584cefb
emilyjette <63060869+emilyjette@users.noreply.github.com>
2020-04-06 11:58:56 -0400

    new activity, buttons

--- app/app/src/main/java/com/emilyjette/memorygame/MainActivity.kt
+++ app/app/src/main/java/com/emilyjette/memorygame/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.emilyjette.memorygame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView.text="Hello "+User.name
     }
-
+    fun onClick(view:View){
+        var intent= Intent(this,GameSActivity::class.java)
+        startActivity(intent)
+    }
 }
