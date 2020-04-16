62b511311bfdfdf8d27cf497b2fe40e355b68634
emilyjette <63060869+emilyjette@users.noreply.github.com>
2020-04-16 11:03:17 -0400

    flash new

--- app/app/src/main/java/com/emilyjette/memorygame/GameSActivity.kt
+++ app/app/src/main/java/com/emilyjette/memorygame/GameSActivity.kt
@@ -21,25 +21,38 @@ class GameSActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_s)
+        setup() //step 1
+        for(count in 1 ..8){
+            greenButton.flash(10  00L*count,oldGreencolor)
+        }
 
+
+//        redButton.flash(500,oldRedcolor)
+//        blueButton.flash(1500,oldBluecolor)
+//        greenButton.flash(2500,oldGreencolor)
+//        yellowButton.flash(3500,oldYellowcolor)
+//        redButton.flash(4500,oldRedcolor)
+//        greenButton.flash(5500,oldGreencolor)
+//        blueButton.flash(6500,oldBluecolor)
+//        yellowButton.flash(7500,oldYellowcolor)
+
+
+
+
+    }
+    fun setup(){
         oldBluecolor= blueButton.backgroundTintList
         oldRedcolor= redButton.backgroundTintList
         oldGreencolor= greenButton.backgroundTintList
         oldYellowcolor= yellowButton.backgroundTintList
-        redButton.flash(500,oldRedcolor)
-        blueButton.flash(1000,oldBluecolor)
-        greenButton.flash(1500,oldGreencolor)
-        yellowButton.flash(2000,oldYellowcolor)
-        redButton.flash(3500,oldRedcolor)
     }
-
     fun ImageButton.flash(time:Long,oldColor:ColorStateList?){
         Handler(Looper.getMainLooper()).postDelayed({
             this.backgroundTintList = resources.getColorStateList(R.color.flash_list_color)
         }, time)
         Handler(Looper.getMainLooper()).postDelayed({
             this.backgroundTintList = oldColor
-        }, time+1000)
+        }, time+500)
 
     }
 }
