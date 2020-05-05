1dc8a87c4317fde7ac133102df9cca15a03e5d39
emilyjette <63060869+emilyjette@users.noreply.github.com>
2020-05-05 11:55:49 -0400

    tried to get table

--- app/app/src/main/java/com/emilyjette/memorygame/StatusActivity.kt
+++ app/app/src/main/java/com/emilyjette/memorygame/StatusActivity.kt
@@ -1,15 +1,24 @@
 package com.emilyjette.memorygame
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.emilyjette.memorygame.User.totalgames
+import kotlinx.android.synthetic.main.activity_status.*
 
 class StatusActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_status)
+        Context.UI_MODE_SERVICE
+        playedStatus.text=""+User.totalgames
+        winStatus.text=""+User.totalwins
+        highStatus.text=""+User.highscore//19?
+        currentStatus.text=""+User.playtimescore//19?
+
         Handler(Looper.getMainLooper()).postDelayed({
             finish()
         }, 8000)
