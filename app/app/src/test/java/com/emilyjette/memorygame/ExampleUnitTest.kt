1e8179951d6c03516bcf845e18a0437c30fcb3aa
emilyjette <63060869+emilyjette@users.noreply.github.com>
2020-05-20 11:36:26 -0400

    wrote some new tests

--- app/app/src/test/java/com/emilyjette/memorygame/ExampleUnitTest.kt
+++ app/app/src/test/java/com/emilyjette/memorygame/ExampleUnitTest.kt
@@ -1,8 +1,10 @@
 package com.emilyjette.memorygame
 
+import kotlinx.android.synthetic.main.activity_game_s.*
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,6 +12,22 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    var game=Game(null)
+
+    companion object {
+        const val Red=1
+        const val Blue=2
+        const val Green=3
+        const val Yellow=4
+    }
+    @Before
+    fun setup(){
+        game.bluegametile.id=Blue
+        game.redgametile.id=Red
+        game.greengametile.id=Green
+        game.yellowgametile.id=Yellow
+    }
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
@@ -18,8 +36,30 @@ class ExampleUnitTest {
     fun `everytime we set up pattern 8 +1 games`(){
         assertEquals(0, User.playtimegames)
 
-        GameSActivity().startAgain()
+        game.setup()
+        game.startAgain()
 
         assertEquals(1, User.playtimegames)
+    }
+    @Test
+    fun `8 correct get +1 wins`(){
+        assertEquals(0,User.playtimewins)
+
+        game.win()
+
+        assertEquals(1,User.playtimewins)
+    }
+    @Test
+    fun `start again score is 0`(){
+        assertEquals(0, User.score)
+
+        game.setup()
+        game.startAgain()
+
+        assertEquals(0, User.score)
+    }
+    @Test
+    fun `if score 8 win`(){
+
     }
 }
